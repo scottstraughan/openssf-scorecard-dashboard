555cb8ce81de18bd50cd1e5edb141248e1ebd06c
Scott Straughan <scott.straughan@codeplay.com>
2024-12-11 12:12:49 +0000

    - Refined how the button works

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,12 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, HostListener, input, model } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'osf-button',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
   icon = input<string | undefined>(undefined);
+  label = input<string | undefined>(undefined);
+  selected = model<boolean>(false);
+
+  @HostListener('click')
+  onClick() {
+    this.selected.set(!this.selected());
+  }
 }
