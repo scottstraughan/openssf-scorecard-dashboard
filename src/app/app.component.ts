c31d991814ee656e4b0d44071af4d7171e09e740
Scott Straughan <scott.straughan@codeplay.com>
2024-12-11 15:32:50 +0000

    - Removed popup showing by default.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal, Signal } from '@angular/core';
+import { Component, signal, Signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { OrganizationModel } from './shared/models/organization.model';
 import { NgClass } from '@angular/common';
@@ -14,7 +14,7 @@ import { AddSecurityKeyPopupComponent } from './add-security-key-popup/add-secur
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   organizations: Signal<OrganizationModel[]> = signal([]);
   selectedOrganization: OrganizationModel | undefined;
 
@@ -24,12 +24,6 @@ export class AppComponent implements OnInit {
   ) {
     this.organizations = toSignal(this.organizationService.getOrganizations(), { initialValue: [] });
     this.setSelectedOrganization(this.organizations()[0]);
-  }
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.onAddAuthorizationKey();
-    });
   }
 
   setSelectedOrganization(organization: OrganizationModel) {
