9b4c90301f57ce4c76aaea8d8157e7c48f5e1bf6
Scott Straughan <scott.straughan@codeplay.com>
2024-12-12 16:26:32 +0000

    Added home view component, mostly used for redirection.

---
+++ src/app/home/home-view.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ServiceStoreService } from '../shared/services/service-store.service';
+import { Router } from '@angular/router';
+import { LoadingComponent } from '../shared/components/loading/loading.component';
+
+@Component({
+  selector: 'app-home-view',
+  standalone: true,
+  templateUrl: 'home-view.component.html',
+  styleUrls: ['./home-view.component.scss'],
+  imports: [
+    LoadingComponent
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeViewComponent {
+  /**
+   * Constructor.
+   * @param serviceStoreService
+   * @param router
+   */
+  constructor(
+    protected serviceStoreService: ServiceStoreService,
+    protected router: Router
+  ) {
+    this.serviceStoreService.getServiceAccounts()
+      .subscribe((accounts) => {
+        const firstAccount = accounts[0];
+
+        this.router.navigate([`/inspect/${firstAccount.service}/${firstAccount.account}`])
+          .then();
+      });
+  }
+}
