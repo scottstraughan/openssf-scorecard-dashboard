756e2aff56187fc1c4e2383160ddabd2fb264c7e
Scott Straughan <scott.straughan@codeplay.com>
2024-12-24 14:58:29 +0000

    Tweaks to repository widget name.

--- src/openssf-dashboard/account-view/views/repository-list-view/repository-list-view.component.ts
+++ src/openssf-dashboard/account-view/views/repository-list-view/repository-list-view.component.ts
@@ -26,7 +26,7 @@ import {
   WritableSignal } from '@angular/core';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
 import { InputComponent } from '../../../shared/components/input/input.component';
-import { RepositoryWidgetComponent } from '../../components/repository/repository-widget.component';
+import { RepositoryWidgetComponent } from '../../components/repository-widget/repository-widget.component';
 import { ScoreRingComponent } from '../../../shared/components/score-ring/score-ring.component';
 import { LoadingComponent } from '../../../shared/components/loading/loading.component';
 import { NgClass } from '@angular/common';
